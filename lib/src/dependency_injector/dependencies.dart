5621cabd4b54e19166042283592d20b701224e82
Flavio Vieira <flavioemerson1@outlook.com>
2023-04-21 13:21:14 -0300

    resolvido problema de bind duplicados

--- lib/src/dependency_injector/dependencies.dart
+++ lib/src/dependency_injector/dependencies.dart
@@ -6,33 +6,40 @@ import 'injector.dart';
 class Dependency<T extends Object> {
   DependenciesRegister<T> bindRegister;
   bool lazyInstance;
+  bool _secondary = false;
 
   Dependency._(
     this.bindRegister,
     this.lazyInstance,
   ) {
     final getIt = GetIt.I;
-    if (lazyInstance) {
-      getIt.registerLazySingleton<T>(() => bindRegister(Injector()));
-    } else {
-      getIt.registerSingleton<T>(bindRegister(Injector()));
+    _secondary = getIt.isRegistered<T>();
+    if (!getIt.isRegistered<T>()) {
+      if (lazyInstance) {
+        getIt.registerLazySingleton<T>(() => bindRegister(Injector()));
+      } else {
+        getIt.registerSingleton<T>(bindRegister(Injector()));
+      }
     }
   }
 
   Dependency._factory(this.bindRegister) : lazyInstance = false {
-    // if (!GetIt.I.isRegistered<T>()) {
-    GetIt.I.registerFactory<T>(() => bindRegister(Injector()));
-    // }
+    _secondary = GetIt.I.isRegistered<T>();
+    if (!GetIt.I.isRegistered<T>()) {
+      GetIt.I.registerFactory<T>(() => bindRegister(Injector()));
+    }
   }
 
   /// Método responsavel por fazer o unregister da factory dentro do GetIT
   void unRegister() {
-    GetIt.I.unregister<T>();
+    if (!_secondary) {
+      GetIt.I.unregister<T>();
+    }
   }
 
   /// O singleton faz com que toda vez que for solicitado uma nova instancia para o gerenciador de dependencias
   /// ele te dará a mesma instancia.
-  /// @param bindRegister nele você deve enviar uma função com o retorno sendo a classe que você gostaria de adicionar ao GetIt
+  /// [bindRegister] nele você deve enviar uma função com o retorno sendo a classe que você gostaria de adicionar ao GetIt
   static Dependency singleton<T extends Object>(
     DependenciesRegister<T> bindRegister,
   ) =>
@@ -41,7 +48,7 @@ class Dependency<T extends Object> {
   /// O Lazy Singleton faz com que toda vez que for solicitado uma nova instancia
   /// para o gerenciador de dependencias ele te dará a mesma instancia, porém diferente do singleton
   /// esse Dependency não inicia a instancia logo no load da página, será criado somente quando for solicitado pela primeira vez.
-  /// @param bindRegister nele você deve enviar uma função com o retorno sendo a classe que você gostaria de adicionar ao GetIt
+  /// [bindRegister] nele você deve enviar uma função com o retorno sendo a classe que você gostaria de adicionar ao GetIt
   static Dependency lazySingleton<T extends Object>(
     DependenciesRegister<T> bindRegister,
   ) =>
@@ -49,7 +56,7 @@ class Dependency<T extends Object> {
 
   /// A factory faz com que toda vez que você pedir uma instancia para o gerenciador de dependencias
   /// ele te dara uma nova instancia.
-  /// @param bindRegister nele você deve enviar uma função com o retorno sendo a classe que você gostaria de adicionar ao GetIt
+  /// [bindRegister] nele você deve enviar uma função com o retorno sendo a classe que você gostaria de adicionar ao GetIt
   static Dependency factory<T extends Object>(
     DependenciesRegister<T> bindRegister,
   ) =>
