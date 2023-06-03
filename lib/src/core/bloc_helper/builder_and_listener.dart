84a14cdebdb0a1f06e1f75cf9602d009da4831a6
Flavio Vieira <flavioemerson1@outlook.com>
2023-06-03 13:34:24 -0300

    adicionado builder e listener

---
+++ lib/src/core/bloc_helper/builder_and_listener.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../../flutter_getit.dart';
+
+class BlocBuilder<T extends FlutterGetItBloc> extends StatelessWidget {
+  final Widget Function(T? state) builder;
+  final T? initialData;
+  const BlocBuilder({
+    Key? key,
+    required this.builder,
+    this.initialData,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final stream = T as FlutterGetItBloc<T>;
+    return StreamBuilder<T>(
+      stream: stream.stateOut,
+      initialData: initialData,
+      builder: (context, snapshot) {
+        final state = snapshot.data;
+        return builder.call(state);
+      },
+    );
+  }
+}
+
+listen<T extends FlutterGetItBloc>(void Function(T state)? execute) {
+  final stream = T as FlutterGetItBloc<T>;
+  stream.stateOut.listen(execute);
+}
