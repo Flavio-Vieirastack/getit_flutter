3ccadc7c8be0b44c32757d4e42de7849f27b1c86
Flavio Vieira <flavioemerson1@outlook.com>
2023-07-26 20:46:00 -0300

    feito export do builder

--- lib/src/core/bloc_helper/builder_and_listener.dart
+++ lib/src/core/bloc_helper/builder_and_listener.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../flutter_getit.dart';
 
-class BlocBuilder<T extends FlutterGetItBloc> extends StatelessWidget {
+class BlocBuilder<T extends FlutterGetItBloc<T>> extends StatelessWidget {
   final Widget Function(T? state) builder;
   final T? initialData;
   const BlocBuilder({
